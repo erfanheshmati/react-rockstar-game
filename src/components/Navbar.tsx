e304a5abb22d8e32196b059f917e37ecf895ba0b
Erfan Heshmati <heshmati74@gmail.com>
2024-05-08 19:33:36 +0330

    custom hook for games info

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ const logo = require("../assets/logo.png");
 
 export default function Navbar() {
   return (
-    <HStack>
-      <Image src={logo} boxSize="50px" borderRadius="10px" margin="10px" />
-      <Text fontSize="18px" fontWeight="500">
-        Rockstar Game
-      </Text>
-      <HStack marginLeft="auto" paddingX="10px">
-        <ColorModeSwitch />
+    <HStack justifyContent="space-between" padding="10px">
+      <HStack>
+        <Image src={logo} boxSize="50px" borderRadius="10px" />
+        <Text fontSize="18px" fontWeight="500">
+          Rockstar Game
+        </Text>
       </HStack>
+      <ColorModeSwitch />
     </HStack>
   );
 }
---
+++ src/hooks/useGame.ts
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  },[]);
+
+  return { games, error };
+};
+
+export default useGames;
