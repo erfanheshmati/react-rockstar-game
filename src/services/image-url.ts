ec5ebf2c821c69f5a62afa449c74f03674750ca2
Erfan Heshmati <heshmati74@gmail.com>
2024-05-09 11:03:20 +0330

    game image optimization (fetching cropped images)

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+  // https://media.rawg.io/media/games/d58/d588947f768s856659sdf9876gh0fd8769fh8d99
+  // https://media.rawg.io/media/crop/600/400/games/d58/d588947f768s856659sdf9876gh0fd8769fh8d99
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
