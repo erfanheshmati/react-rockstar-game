b4fc156dac75b1e5424cfd5971062819d0f14cb1
Erfan Heshmati <heshmati74@gmail.com>
2024-05-13 18:18:56 +0330

    games pagination

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,19 @@
-import { SimpleGrid, Text, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  SimpleGrid,
+  Spacer,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
+import { useState } from "react";
 
 interface Props {
   gameQuery: GameQuery;
@@ -12,6 +22,9 @@ interface Props {
 export default function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(8);
+
   let skeletons = [];
   for (let i = 1; i <= data.length; i++) {
     skeletons.push(i);
@@ -19,6 +32,13 @@ export default function GameGrid({ gameQuery }: Props) {
 
   const toast = useToast();
   const id: string = "";
+
+  /* Pagination */
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  const paginate = (pageNumber: any) => setCurrentPage(pageNumber);
+  const isActive = (idx: any) => (currentPage === idx + 1 ? true : false);
 
   return (
     <>
@@ -37,12 +57,28 @@ export default function GameGrid({ gameQuery }: Props) {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
+        {currentItems.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
+
+      {/* Pagination */}
+      <Box width={"fit-content"} marginX={"auto"} marginY={10}>
+        {Array.from({ length: Math.ceil(data.length / itemsPerPage) }).map(
+          (_, index) => (
+            <Button
+              key={index + 1}
+              onClick={() => paginate(index + 1)}
+              marginX={1}
+              isActive={isActive(index)}
+            >
+              {index + 1}
+            </Button>
+          )
+        )}
+      </Box>
     </>
   );
 }
